06301b398e1d56192c5690cdf520c97b75b64d75
Javi Cerezo <jc.webmob@gmail.com>
2022-08-29 13:38:29 +0200

    Primer commit, TODO POR HACER.

---
+++ includes/app.php
@@ -0,0 +1,4 @@
+<?php
+
+    define('TEMPLATE_URL', __DIR__ . '/templates.php');
+    define('FUNCIONES_URL', __DIR__ . '/funciones.php');
---
+++ includes/funciones.php
@@ -0,0 +1,6 @@
+<?php
+    require 'app.php';
+
+    function incluirTemplate (string $nombre, bool $inicio = false) {
+        include TEMPLATE_URL . "/${nombre}.php";
+    }
